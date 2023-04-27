24449d053abe5bb4817bdfcf0f887ab5c425aeb6
guoxuan.wang <61870782+HamsterCat-W@users.noreply.github.com>
2023-04-27 19:25:11 +0800

    chore: 基础按钮组件

---
+++ src/components/button.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button>smart-btn</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'SButton',
+  props: {},
+  setup() {
+    return {}
+  },
+})
+</script>
